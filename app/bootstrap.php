e16af06f5a50c13f6bd48418a0e785c865e962dd
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2017-06-19 02:36:07 +0200

    remove hardcoded 'display_errors' from bootstrap

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,8 +4,6 @@ use Accorlex\Article\Controller\ArticleController;
 use Silex\Application;
 use Silex\Provider\ServiceControllerServiceProvider;
 
-ini_set('display_errors', 1);
-
 $app = new Application;
 $app->register(new ServiceControllerServiceProvider);
 $app['debug'] = $appDebug;
