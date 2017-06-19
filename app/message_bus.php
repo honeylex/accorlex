8b184925bf9aa24f40f2aea268d7fda29737d207
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2017-06-19 03:49:28 +0200

    tidy up the msb bootstrapping a bit

--- app/message_bus.php
+++ app/message_bus.php
@@ -23,6 +23,7 @@ use Accorlex\Article\Projection\ArticleProjectionType;
 use Accorlex\MessageBus\CallbackHandler;
 use Accorlex\MessageBus\LazyHandler;
 
+// /dev/null style persistence implementation for the CommitStream
 final class EchoPersistenceAdapter implements PersistenceAdapterInterface
 {
     /**
@@ -47,32 +48,31 @@ final class EchoPersistenceAdapter implements PersistenceAdapterInterface
         return true;
     }
 }
-//
-$persistenceAdapter = new EchoPersistenceAdapter;
+
+// used to pass the message-bus by reference to any command-handler factories
 $messageBus = null;
-$commandHandlerFactory = function () use (&$messageBus, $persistenceAdapter) {
-    return new CreateArticleHandler(
-        new ArticleEntityType,
-        new UnitOfWork(Article::class, $persistenceAdapter),
-        $messageBus
-    );
+// create unit-of-work for article AR's
+$articleUow = new UnitOfWork(Article::class, new EchoPersistenceAdapter);
+// create in-process transport used for all subscriptions
+$transport = new InProcessTransport("inproc");
+$commandHandlerFactory = function () use (&$messageBus, $articleUow) {
+    return new CreateArticleHandler(new ArticleEntityType, $articleUow, $messageBus);
 };
 // setup command channel
-$inProc = new InProcessTransport("inproc");
 $commandHandlers = new MessageHandlerList([ new LazyHandler($commandHandlerFactory) ]);
-$commandSub = new Subscription("command-sub", $inProc, $commandHandlers);
+$commandSub = new Subscription("command-sub", $transport, $commandHandlers);
 $commandChannel = new Channel("commands", new SubscriptionMap([ $commandSub ]));
 // setup commit channel
 $commitHandlers = new MessageHandlerList([ new StandardProjector(new ArticleProjectionType) ]);
-$commitSub = new Subscription("commit-sub", $inProc, $commitHandlers);
+$commitSub = new Subscription("commit-sub", $transport, $commitHandlers);
 $commitChannel = new Channel("commits", new SubscriptionMap([ $commitSub ]));
 // setup event channel
 $eventHandlers = new MessageHandlerList([ new CallbackHandler(function (EnvelopeInterface $envelope): bool {
     echo "<h4>Received message '".get_class($envelope->getMessage())."' on (post-commit)event channel, but not doing anything with it atm.</h4>";
     return true;
 }) ]);
-$eventSub = new Subscription("event-sub", $inProc, $eventHandlers);
+$eventSub = new Subscription("event-sub", $transport, $eventHandlers);
 $eventChannel = new Channel("events", new SubscriptionMap([ $eventSub ]));
-// initialize message-bus with channels
+// initialize message-bus
 $messageBus = new MessageBus(new ChannelMap([ $commandChannel, $commitChannel, $eventChannel ]));
 return $messageBus;
